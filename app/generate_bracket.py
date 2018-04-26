f8ddba0b4383f5cb9c8c6a794eebddc38a127230
sba29 <berkhoutalex@gmail.com>
2018-04-26 17:39:36 -0500

    testing with tourney order

--- app/generate_bracket.py
+++ app/generate_bracket.py
@@ -106,7 +106,7 @@ def get_actual_results(season):
 def get_tourney_order(season):
     tourney_order = []
     for x in seeds1:
-        tourney_order.append(x,season)
+        tourney_order.append(x)
     return tourney_order
 
 # returns results from tournament given a set of indicators
